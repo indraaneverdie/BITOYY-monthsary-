0e94a8606b0cd713d12b3ac7965bf624e8bb68b4
indraaneverdie <indraaneverdie@gmail.com>
2026-07-17 00:19:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,48 @@ function showLove(){
 alert("Happy Monthsary ❤️\n\nI love you forever, Fathima. Thank you for making my life beautiful.");
 }
 
+
+// Floating Hearts ❤️
+
+const hearts = [
+"❤️",
+"💕",
+"💖",
+"💗",
+"💘"
+];
+
+function createHeart(){
+
+const heart = document.createElement("div");
+
+heart.className = "heart";
+
+heart.innerHTML = hearts[Math.floor(Math.random()*hearts.length)];
+
+heart.style.left = Math.random()*100 + "vw";
+
+heart.style.animationDuration = (Math.random()*3+3)+"s";
+
+document.body.appendChild(heart);
+
+
+setTimeout(()=>{
+
+heart.remove();
+
+},6000);
+
+}
+
+setInterval(createHeart,500);
+
+
+
+// Timer ⏳
+
 const start = new Date("May 17, 2026");
+
 
 function updateTimer(){
 
@@ -14,17 +55,29 @@ const days = Math.floor(diff/(1000*60*60*24));
 
 document.getElementById("timer").innerHTML =
 days + " days together ❤️";
+
 }
 
 setInterval(updateTimer,1000);
 
 updateTimer();
-function toggleMusic() {
-    const music = document.getElementById("music");
 
-    if (music.paused) {
-        music.play();
-    } else {
-        music.pause();
-    }
+
+
+// Music 🎵
+
+function toggleMusic() {
+
+const music = document.getElementById("music");
+
+if (music.paused) {
+
+music.play();
+
+} else {
+
+music.pause();
+
+}
+
 }
